5c2ebc6e35cd9c165bcf5641f137cb5c2c168b67
Marcin Bugaj <33054342+schmitshny@users.noreply.github.com>
2024-02-22 21:05:51 +0100

    Update userRoutes.ts

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -5,23 +5,20 @@ import { validateUserUpdate } from '../middlewares/validateUserUpdate';
 
 export const userRoutes = (): Router => {
   const router = Router();
-  router.get('/get-users', authMiddleware, UserController.getAllUsers);
+  router.get('/get-users', UserController.getAllUsers);
   router.put(
     '/update-avatar/:id',
-    authMiddleware,
     UserController.updateUserAvatar,
   );
   router.get(
     '/search-users/:searchTerm',
-    authMiddleware,
     validateUserUpdate,
     UserController.searchUsers,
   );
-  router.put('/update-user/:id', authMiddleware, UserController.updateUser);
-  router.get('/get-user/:id', authMiddleware, UserController.getUserById);
+  router.put('/update-user/:id', UserController.updateUser);
+  router.get('/get-user/:id', UserController.getUserById);
   router.put(
     '/update-user-status/:id',
-    authMiddleware,
     UserController.updateUserStatus,
   );
   return router;
